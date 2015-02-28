8681705c37f40f22e62c287689704fd673e53699
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2015-02-27 23:14:47 -0500

    got threads running

--- assign2.c
+++ assign2.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -5,6 +6,8 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 #include "parser.h"
 //#include "listen.h"
 //uint16_t command;
@@ -14,33 +17,62 @@
 //uint32_t address;
 //} entries[num_entries];
 
+struct ripEntry{
+	char destVIP[30];
+	struct interface nextHop;
+	int cost;
+	time_t updateTime;
+	bool upDown;
+	struct ripEntry* next;
+	struct ripEntry* prev;
+}ripEntry;
+
+struct ripTable{
+	struct interface interfaceList;
+	struct nodeInfo mainNode; 
+}ripTable;
+struct threadInit{
+	struct nodeInfo mainNode;
+	
+};
 void* reader(void* data){
 	struct returnInfo* allInfo = (struct returnInfo*) data;
-	struct nodeInfo* = allInfo -> mainNode;
-	struct interface* intList = allInfo -> interface;
-	printf("RRREEAADDD");
+	struct nodeInfo* mainNode = allInfo -> mainNode;
+	struct interface* intList = allInfo -> interfaceList;
+	printf("Read thread run \n");
 	return NULL;
 }
 void* listener(void* data){
 	struct returnInfo* allInfo = (struct returnInfo*) data;
-        struct nodeInfo* = allInfo -> mainNode;
-        struct interface* intList = allInfo -> interface;
-	printf("Listener");
+        struct nodeInfo* mainNode= allInfo -> mainNode;
+        struct interface* intList = allInfo -> interfaceList;
+	printf("Listener thread run \n");
 	return NULL;
 }
 int main(int argc, char ** argv){
 	struct returnInfo* returnData = parser(argc, argv);
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 	pthread_t* readThread = (pthread_t *)malloc(sizeof(pthread_t));
 	pthread_t* listenThread = (pthread_t*) malloc(sizeof(pthread_t));
-	if(pthread_create(readThread, NULL, reader, (void*)returnData)){
-		printf("thread creation error\n");	
+	int rc = 0;
+	if(rc = pthread_create(readThread, &attr, reader, (void*)returnData)){
+		printf("thread creation error %d\n", rc);	
 	}
-	if(pthread_create(listenThread, NULL, listener,(void*)returnData)){
-                printf("thread creation error\n");
+	if(rc = pthread_create(listenThread, &attr, listener,(void*)returnData)){
+                printf("thread creation error%d\n", rc);
         }
+char buffer[256]; 
+while(1){
+	scanf("%s", buffer);
+//	printf("%s \n", buffer);
+	if(!strcmp(buffer, "ifconfig")){
+		printf("Yay! config!");
+	}
+}
 
 
-
-pthread_join(*readThread,NULL);
-pthread_join(*listenThread, NULL);
+//pthread_join(*readThread,NULL);
+//pthread_join(*listenThread, NULL);
 }
